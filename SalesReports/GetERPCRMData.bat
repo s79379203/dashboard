8bc92d700dcf8668d7bd426f44edf99da65248e2
s79379203 <76547452+s79379203@users.noreply.github.com>
2022-06-22 11:28:37 +0800

    Add files via upload

---
+++ SalesReports/GetERPCRMData.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\R\R-4.1.0\bin\x64\R.exe" CMD BATCH "C:\Users\elvis\Documents\fwt_reports\SalesReports\markdown.R"
+
+copy   C:\Users\elvis\Documents\fwt_reports\SalesReports\SalesDashboards.html Y:\SalesReports
+
+copy   C:\Users\elvis\Documents\fwt_reports\SalesReports\custpo.RData Y:\SalesReports
